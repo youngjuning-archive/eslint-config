c93c6a709e26a208942d762dafb3be910657a488
youngjuning <youngjuning@aliyun.com>
2020-12-02 16:54:51 +0800

    chore: edit javascript rule

--- overrides/rules/javascript.js
+++ overrides/rules/javascript.js
@@ -8,12 +8,15 @@ module.exports = {
       exceptAfterSingleLine: true, //  跳过对单行类成员之后的空行的检查
     },
   ],
+  'max-classes-per-file': 0,
+  'class-methods-use-this': 0,
+  'no-plusplus': 0,
   'no-unused-vars': 1,
   'no-useless-constructor': 1,
-  'max-classes-per-file': 0,
   'no-nested-ternary': 0, // 禁止使用嵌套的三元表达式
   'no-restricted-globals': 0, // 禁用特定的全局变量
   'no-use-before-define': 0, // 禁止定义前使用
+  'no-underscore-dangle': 0, // 禁止标识符中有悬空下划线
   'no-unused-expressions': 0, // 禁止未使用过的表达式
   'prefer-destructuring': [
     2,
